6fe15aa5f4abb0ebb23758702ef8acfd763ae667
1OSCARFONSECA1 <oifonseca@uniboyaca.edu.co>
2018-11-17 20:02:34 -0500

    [Add] NavBar

--- Software/views/modules/navegacion.php
+++ Software/views/modules/navegacion.php
@@ -1,8 +1,19 @@
-<nav>
-	<ul>
-		<li><a href="index.php">Registro</a></li>
-		<li><a href="index.php?action=ingresar">Ingreso</a></li>
-		<li><a href="index.php?action=usuarios">Usuarios</a></li>
-		<li><a href="index.php?action=salir">Salir</a></li>
-	</ul>
-</nav>
+<div class="topnav" id="myTopnav">
+  <a href="#home" class="active">Inicio</a>
+  <a href="#news">Dependencia Administrativa</a>
+  <a href="#contact">Dependencia Academica</a>
+  <a href="#about">Consulta</a>
+  <div class="dropdown">
+    <button class="dropbtn">Ingreso / Registro 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+<a href="index.php">Registro</a>
+<a href="index.php?action=ingresar">Ingreso</a>
+<a href="index.php?action=usuarios">Usuarios</a>
+<a href="index.php?action=salir">Salir</a>
+    </div>
+  </div> 
+  
+  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="navbar()">&#9776;</a>
+</div>
