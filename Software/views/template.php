9d9646f646ad38c06d417c2424dace2517e403c0
1OSCARFONSECA1 <oifonseca@uniboyaca.edu.co>
2018-11-17 20:20:23 -0500

    [Fix] Template

--- Software/views/template.php
+++ Software/views/template.php
@@ -4,13 +4,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Vicerrectoria - Universidad de Boyacá</title>
+	<!-- styles -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="views/css/normalize.css">
 	<link rel="stylesheet" type="text/css" href="views/css/styles.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script type="text/javascript" src="views/js/site.js"></script>
 </head>
-
 <body>
 	<?php include "modules/navegacion.php"; ?>
 	<section>
@@ -20,5 +18,8 @@
 		 ?>
 	</section>
 	<?php include "modules/footer.php"; ?>
+	<!-- Scripts -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script type="text/javascript" src="views/js/site.js"></script>
 </body>
 </html>
