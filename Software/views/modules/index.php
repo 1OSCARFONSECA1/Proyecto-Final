91daea8de2748f3e3c53d22a6ea10620b0e6740a
1OSCARFONSECA1 <oifonseca@uniboyaca.edu.co>
2018-11-17 20:13:24 -0500

    [Add] Index Elements

---
+++ Software/views/modules/index.php
@@ -0,0 +1,4 @@
+<p class="lead text-white">Bienvenido al control de la Vicerrectoría académica</p>
+<figure>
+<img src="./views/images/uniboyaca.png" style="width: 200px;" class="img-fluid">
+</figure>
