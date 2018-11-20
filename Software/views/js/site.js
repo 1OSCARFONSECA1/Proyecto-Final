b4628e1f12efc4da8a1103657fd779d819c1708f
1OSCARFONSECA1 <oifonseca@uniboyaca.edu.co>
2018-11-19 22:57:13 -0500

    Add elements academic

--- Software/views/js/site.js
+++ Software/views/js/site.js
@@ -6,3 +6,40 @@ function navbar() {
         x.className = "topnav";
     }
 }
+
+
+$("select#typeMain").change(function () {
+    if(this.value == "facultad"){
+        $("#addHijito").show();
+    }else{
+        $("#addHijito").hide();
+        $("#childBox").remove();
+    }
+});
+
+
+$("select#element").change(function () {
+    var text = "";
+    if(this.value == "programa"||this.value == "departamento"){
+        text = '<center>'+
+        '<input type="text" name="code_'+this.value+'" placeholder="Código del '+this.value+'">'+
+        '<label>Nombre</label><input type="text" name="name_'+this.value+'" placeholder="nombre del '+this.value+'">'+
+        '<label>Sigla</label><input type="text" name="sigla_'+this.value+'" placeholder="sigla del '+this.value+'">'+
+        '<label>Activo: </label><input type="checkbox" name="active_'+this.value+'" value="1" checked=""></center>';
+    }else{
+        $("#childBox").remove();
+    }
+    $("#childBox").html(text);
+});
+
+
+
+$('#addHijito').on('click',function(e){
+    $("#hijito").show();
+});
+
+
+$('#addCalidad').on('click',function(e){
+    $("#calidad").show();
+});
+
--- Software/views/modules/academico.php
+++ Software/views/modules/academico.php
@@ -1,33 +1,62 @@
-    <h1>Herramientas de La Dependencia Academica</h1>
-    <p>Información: La aplicación debe permitir que se gestionen (adicionen, modifiquen e inactiven) las dependencias académicas para lo cual se requieren los siguientes datos: código de la dependencia, nombre y sigla.</p>
+<h1>Herramientas de La Dependencia Academica</h1>
+<p>Información: La aplicación debe permitir que se gestionen (adicionen, modifiquen e inactiven) las dependencias académicas para lo cual se requieren los siguientes datos: código de la dependencia, nombre y sigla.</p>
 <br/>
-  <form method="post">
-<input type="hidden" name="main" value="Academico" />
-  <center>
-  <label>Código de </label>
-  <select name="type">
-  <option value="Seleccione" selected>Seleccione</option>
-  <option value="facultad">facultad</option>
-  <option value="programa">programa</option>
-  <option value="departamento">departamento</option>
-</select>
-  <input type="text" name="code" placeholder="Código" />
-  <label>Nombre</label>
-  <input type="text" name="name" placeholder="nombre" />
-  <label>Sigla</label>
-  <input type="text" name="sigla" placeholder="sigla" />
-  <label>Activo: </label><input type="checkbox" name="active" value="1" checked>
-  <br/><br/><hr/>
-  <label>Adicionar</label>
-  <select id="element">
-  <option value="Seleccione" selected>Seleccione</option>
-  <option value="programa">programa</option>
-  <option value="departamento">departamento</option>
-</select>
-<br/><hr/><br/>
-  <button name="add" type="submit">Adicionar</button>
-  <button name="list" type="submit">Lista</button>
-  </center>
+<form method="post">
+    <input type="hidden" name="main" value="Academico" />
+    <center>
+        <label>Código de </label>
+        <select name="type" id="typeMain">
+            <option value="Seleccione" selected>Seleccione</option>
+            <option value="facultad">facultad</option>
+            <option value="programa">programa</option>
+            <option value="departamento">departamento</option>
+        </select>
+        <input type="text" name="code" placeholder="Código" />
+        <label>Nombre</label>
+        <input type="text" name="name" placeholder="nombre" />
+        <label>Sigla</label>
+        <input type="text" name="sigla" placeholder="sigla" />
+        <label>Activo: </label><input type="checkbox" name="active" value="1" checked>
+        <br/>
+        <hr/><br/>
+        <button id="addHijito" style="display:none" type="button">Adicionar Facultad (Hijo)</button>
+        <button id="addCalidad" type="button">Adicionar Proceso de Calidad</button>
+        <br/><br/>
+        <div id="hijito">
+            <hr/> Adicionar hijo:
+            <label>Adicionar</label>
+            <select id="element">
+              <option value="Seleccione" selected>Seleccione</option>
+              <option value="programa">programa</option>
+              <option value="departamento">departamento</option>
+            </select>
+            <div id="childBox"></div>
+        </div>
+        <hr/>
+        <div id="calidad">
+        <h3>Proceso de calidad</h3>
+            <label>Añadir </label> Registro Calificado, Acreditación y auto evaluación
+            <select name="type">
+              <option value="Seleccione" selected>Seleccione</option>
+              <option value="Registro_Calificado">Registro Calificado</option>
+              <option value="Acreditacion">Acreditación</option>
+              <option value="Auto_Evaluacion">Auto-Evaluacion</option>
+            </select>
+            <div id="optionOne">
+                <label>Numero de Resolución (MEN):<label> <input type="number" name="numberMEN" placeholder="Número" />
+                <label>Tiempo de Vigencia:</label> <input type="date" name="vigencia">
+            </div>
+            <div id="optionTwo">
+            <label>Fecha AutoEvaluaciónn: </label> <input type="date" name="autoevaluacion">
+            <label>Fecha Proxima Autoevaluación:</label> <input type="date" name="renovacion">
+            </div>
+            <hr/>
+        </div>
+        <br/>
+        <button name="add" type="submit">Adicionar</button>
+        <button name="list" type="submit">Lista</button>
+        <br/><br/>
+    </center>
 </form>
 <br/>
 <p><strong>Las dependencias académicas son:</strong> Las facultades, los programas y los departamentos. <i>Una facultad puede tener uno o más programas y uno o más
