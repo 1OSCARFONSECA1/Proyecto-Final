b33d2d175d8d7683454966f80f637efe8b9da1e4
1OSCARFONSECA1 <43160728+1OSCARFONSECA1@users.noreply.github.com>
2018-11-22 13:50:02 -0500

    Update footer.php

--- Software/views/modules/footer.php
+++ Software/views/modules/footer.php
@@ -1,4 +1,18 @@
 <footer>
-  <p>Oscar Ivan Fonseca</p>
-  <p>Contacto: <a href="mailto:oifonseca@uniboyaca.edu.co">oifonseca@uniboyaca.edu.co</a>.</p>
+  <footer>
+    <p>Universidad de Boyacá | Vigilada MinEducación</p>
+    <p>Nit. 891801101-6</a>.</p>
+    <p>Personería Jurídica N° 6553 de mayo de 1981 - Resolución N° 2910 del 16 de septiembre de 2004 MEN </p>
+    <p>© Derechos reservados Universidad de Boyacá </p>
+
+    <a href="https://es-la.facebook.com/universidaddeboyaca/"><img src="https://campusvirtual.uniboyaca.edu.co/express/design/imgs/face.png" alt="" role="presentation" class="img-responsive atto_image_button_text-bottom" data-pin-nopin="true" width="26" height="26"></a>
+  </a>
+  <a href="https://www.instagram.com/universidad_de_boyaca/"><img src="https://campusvirtual.uniboyaca.edu.co/express/design/imgs/instagram.png" alt="" role="presentation" class="img-responsive atto_image_button_text-bottom" data-pin-nopin="true" width="26" height="26">
+</a>
+<a href="https://twitter.com/univboyaca"><img src="https://campusvirtual.uniboyaca.edu.co/express/design/imgs/twitter.png" alt="" role="presentation" class="img-responsive atto_image_button_text-bottom" data-pin-nopin="true" width="26" height="26"></a>
+</a>
+<a href="https://www.youtube.com/user/universidaddeboyaca"><img src="https://campusvirtual.uniboyaca.edu.co/express/design/imgs/youtube.png" alt="" role="presentation" class="img-responsive atto_image_button_text-bottom" data-pin-nopin="true" width="26" height="26"></a>
+
+  </footer>
+
 </footer>
